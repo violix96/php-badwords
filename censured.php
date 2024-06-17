ad0661916251687a60ce17552e35ac4627b21245
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-06-17 19:23:57 +0200

    [UPD] added style

--- censured.php
+++ censured.php
@@ -14,18 +14,19 @@ $censoredLength = strlen($censoredParagraph);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
     <title>Risultati</title>
 </head>
 
 <body>
-    <div class="container ">
+    <div class="d-flex justify-content-center align-items-center vh-100">
         <div class="row">
             <div class="col-12 text-center">
-                <h1>Stampa de paragrafi</h1>
-                <p>Il testo originale è: <?php echo $badwords ?></p>
-                <p>La lunghezza del testo originale è: <?php echo $originalLength ?> caratteri</p>
-                <p>La parola censurata è: <?php echo $censoredParagraph ?></p>
-                <p>La lunghezza della parola censurata è: <?php echo $censoredLength ?> caratteri</p>
+                <h1 class="py-5">Stampa dei paragrafi</h1>
+                <p>Il testo originale è: <span class="text-white text-danger fs-5"><?php echo $badwords ?></span></p>
+                <p>La lunghezza del testo originale è: <span class="text-danger text-danger fs-5"><?php echo $originalLength ?></span> <span class="text-white text-danger fs-5">caratteri</span></p>
+                <p>La parola censurata è: <span class="text-danger fs-5"><?php echo $censoredParagraph ?></span></p>
+                <p>La lunghezza della parola censurata è: <span class="text-danger text-danger fs-5"><?php echo $censoredLength ?> <span class="text-white text-danger fs-5">caratteri</span></p>
             </div>
         </div>
     </div>
